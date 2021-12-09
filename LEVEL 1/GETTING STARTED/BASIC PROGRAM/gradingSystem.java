d125496a1aaac2713ccd54e598a2660feabe457e
Kartikey246 <59953234+Kartikey246@users.noreply.github.com>
2021-12-09 14:17:17 +0530

    Update gradingSystem.java
    if and else if mein differnece

--- LEVEL 1/GETTING STARTED/BASIC PROGRAM/gradingSystem.java
+++ LEVEL 1/GETTING STARTED/BASIC PROGRAM/gradingSystem.java
@@ -10,7 +10,8 @@ import java.util.*;
       Scanner scn = new Scanner(System.in);
       int marks = scn.nextInt();
       // input - don''t change this code
-      
+      // bina and wali condition ke bhi kaaam chal jayega bcoz pehli condition ne bound kar diya hai 90 se
+    // agar mein sirf if use karta kar conditioin ko check karne ke liye toh mujhe and ki jarurat hoti 
       // code here 
       if (marks > 90){
           System.out.println("excellent");
