f424d459601a7d57bf00f11378b67e1ef2f1d087
Kartikey Vishnoi <59953234+Kartikey246@users.noreply.github.com>
2022-01-12 19:33:16 +0530

    Create Find Smallest Letter Greater Than Target.java

---
+++ Leetcode/Find Smallest Letter Greater Than Target.java
@@ -0,0 +1,21 @@
+/*
+Given a characters array letters that is sorted in non-decreasing order and a character target, return the smallest character in the array that is larger than target.
+
+Note that the letters wrap around.
+
+For example, if target == 'z' and letters == ['a', 'b'], the answer is 'a'.
+*/
+
+
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int lo = 0, hi = letters.length;
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (letters[mi] <= target) lo = mi + 1;
+            else hi = mi;
+        }
+        return letters[lo % letters.length];
+    }
+}
