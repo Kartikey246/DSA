455e7cd0bfdb4c6afdc21504a155e5bea677c9a3
Kartikey Vishnoi <59953234+Kartikey246@users.noreply.github.com>
2022-01-18 12:22:23 +0530

    Update Kth missing Positive Number.java

--- Leetcode/Kth missing Positive Number.java
+++ Leetcode/Kth missing Positive Number.java
@@ -28,3 +28,17 @@ class Solution {
 }
 
  
+Assume the final result is x,
+And there are m number not missing in the range of [1, x].
+Binary search the m in range [0, A.size()].
+
+If there are m number not missing,
+that is A[0], A[1] .. A[m-1],
+the number of missing under A[m] is A[m] - 1 - m.
+
+If A[m] - 1 - m < k, m is too small, we update left = m.
+If A[m] - 1 - m >= k, m is big enough, we update right = m.
+
+Note that, we exit the while loop, l = r,
+which equals to the number of missing number used.
+So the Kth positive number will be l + k.
